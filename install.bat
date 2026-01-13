8608082ad8e1079e0ea9e6bf383212dc53990e16
Th4End <eliazidb@gmail.com>
2026-01-13 11:15:54 +0100

    feat: add installation scripts for Windows and Unix

---
+++ install.bat
@@ -0,0 +1,6 @@
+python -m venv venv
+venv\Scripts\activate
+pip install -r requirements.txt
+chmod +x install.bat
+echo "Installation complete."
+py cam.py
---
+++ install.sh
@@ -0,0 +1,6 @@
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+chmod +x run.sh
+echo "Installation complete."
+python3 cam.py
